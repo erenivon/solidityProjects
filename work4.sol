60e07a37088e31361bc6bfe3a1728744eacee226
Sherzed <42775426+sherzed@users.noreply.github.com>
2022-05-21 00:26:06 +0300

    Update work4.sol

--- work4.sol
+++ work4.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 contract AirConditioing
 {
    uint256 [4] public paidToken;
-   uint256 [4] ac; //klima
+   uint256 [4] ac; //klima_id
    uint256 [4] public ac_degree; //derece
    address [4] public wallet; //klima sahibi
       
@@ -12,12 +12,12 @@ contract AirConditioing
    ////////////MAIN////////////
    function setAcOwner(uint256 selectedAC,uint256 tokenValue , uint256 _degree ) public returns(uint256)
    {
-    require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
-    require(paidToken[selectedAC]<tokenValue,"Don't be afraid to take risks, increase the price :)");
-    paidToken[selectedAC]=tokenValue;
-    ac_degree[selectedAC]=_degree;
-    wallet[selectedAC] = msg.sender;
-    return paidToken[selectedAC];
+        require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
+        require(paidToken[selectedAC]<tokenValue,"Don't be afraid to take risks, increase the price :)");
+        paidToken[selectedAC]=tokenValue;
+        ac_degree[selectedAC]=_degree;
+        wallet[selectedAC] = msg.sender;
+        return paidToken[selectedAC];
    }
 
    function SetDegree(uint256 selectedAC, uint256 _degree) public 
