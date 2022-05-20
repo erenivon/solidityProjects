3caf2f684247cc6bf7652bb53c038c3cb1676c07
Sherzed <42775426+sherzed@users.noreply.github.com>
2022-05-21 00:23:58 +0300

    Update work4.sol

--- work4.sol
+++ work4.sol
@@ -3,28 +3,28 @@ pragma solidity ^0.8.7;
 
 contract AirConditioing
 {
-   uint256 [] public paidToken;
-   uint256 [] public ac; //klima
-   uint256 [] public ac_degree; //derece
-   address [] public wallet; //klima sahibi
+   uint256 [4] public paidToken;
+   uint256 [4] ac; //klima
+   uint256 [4] public ac_degree; //derece
+   address [4] public wallet; //klima sahibi
       
    //
    ////////////MAIN////////////
-   function setAcOwner(uint256 _ac,uint256 tokenValue , uint256 _degree ) public returns(uint256)
+   function setAcOwner(uint256 selectedAC,uint256 tokenValue , uint256 _degree ) public returns(uint256)
    {
-    require(ac[_ac]>0&&ac[_ac]<5,"We only have 4 air conditioners :( Please choose between 1-4.");
-    require(paidToken[_ac]<tokenValue,"Don't be afraid to take risks, increase the price :)");
-    paidToken[_ac]=tokenValue;
-    ac_degree[_ac]=_degree;
-    wallet[_ac] = msg.sender;
-    return paidToken[_ac];
+    require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
+    require(paidToken[selectedAC]<tokenValue,"Don't be afraid to take risks, increase the price :)");
+    paidToken[selectedAC]=tokenValue;
+    ac_degree[selectedAC]=_degree;
+    wallet[selectedAC] = msg.sender;
+    return paidToken[selectedAC];
    }
 
-   function SetDegree(uint256 _ac , uint256 _degree) public 
+   function SetDegree(uint256 selectedAC, uint256 _degree) public 
     {
-        require(ac[_ac]>0&&ac[_ac]<5,"We only have 4 air conditioners :( Please choose between 1-4.");
-        require(wallet[_ac] == msg.sender, "The owner of the air conditioner does not appear here.");
+        require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
+        require(wallet[selectedAC] == msg.sender, "The owner of the air conditioner does not appear here.");
         require(_degree>15&&_degree<31,"Values must be between 16-30.");
-        ac_degree[_ac]=_degree;
+        ac_degree[selectedAC]=_degree;
     }
 }
