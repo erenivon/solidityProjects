f8c9957798efcb5383fdd8a3ff1a5dfb81313524
Sherzed <42775426+sherzed@users.noreply.github.com>
2022-05-21 15:28:34 +0300

    Update work5-interface.sol

--- work5-interface.sol
+++ work5-interface.sol
@@ -12,25 +12,25 @@ contract SetAirConditioing is AirConditioings {
     event Last(address owner);
     uint256 [4] paidToken; uint256 [4] ac_degree; address [4] wallet;
     function getTokenValue(uint256 selectedAC) public view override returns (uint256) {
-        return paidToken[selectedAC];
+        return paidToken[selectedAC-1];
     }
     function getAdmin(uint256 selectedAC) public view override returns (address) {
-        return wallet[selectedAC];
+        return wallet[selectedAC-1];
     }
     function getDegree(uint256 selectedAC) public view override returns (uint256) {
-        return ac_degree[selectedAC];
+        return ac_degree[selectedAC-1];
     }
    function setAdmin(uint256 selectedAC, uint256 tokenValue) public override {
        require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
-        require(paidToken[selectedAC]<tokenValue,"Don't be afraid to take risks, increase the price :)");
-        paidToken[selectedAC]=tokenValue; wallet[selectedAC] = msg.sender;
+        require(paidToken[selectedAC-1]<tokenValue,"Don't be afraid to take risks, increase the price :)");
+        paidToken[selectedAC-1]=tokenValue; wallet[selectedAC-1] = msg.sender;
         emit Last(msg.sender); 
-        lastPayyer = wallet[selectedAC];
+        lastPayyer = wallet[selectedAC-1];
    }
    function setDegree(uint256 selectedAC, uint256 _degree) public override {
         require(selectedAC>0&&selectedAC<5,"We only have 4 air conditioners :( Please choose between 1-4.");
-        require(wallet[selectedAC] == msg.sender, "The owner of the air conditioner does not appear here.");
+        require(wallet[selectedAC-1] == msg.sender, "The owner of the air conditioner does not appear here.");
         require(_degree>15&&_degree<33,"Values must be between 16-30.");
-        ac_degree[selectedAC]=_degree;
+        ac_degree[selectedAC-1]=_degree;
    }
  }
