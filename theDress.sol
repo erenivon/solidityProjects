83fc47de70bcf8b226f926a5fcabf2fbee67957f
Eren Yılmaz <42775426+sherzed@users.noreply.github.com>
2022-06-16 18:30:33 +0300

    Update theDress.sol

--- theDress.sol
+++ theDress.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract theDress{
      uint256 STEP = 5 minutes;
+    // IERC20 private dressToken;
      uint256 public immutable_start_time;
 
      constructor(/*address tokenAddress*/) {
+        //  dressToken = IERC20(tokenAddress);
          immutable_start_time = block.timestamp;
          PRICE = 1 ether/1000;
      }
 
      struct Voter {
-        //bool voted;
         uint vote;
         bool votedBlue;
         bool votedGold;
@@ -33,26 +34,23 @@ contract theDress{
     uint256 passingTime;
     uint256 timeDifference;
     uint256 total_ownerPercentage;
-    address _chairperson = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address _chairperson = 0x6a411Be2a84eaf31d9F6092CA08F364Fb9Fe1350;
 
-    mapping(address=>uint256) public addressToAmountFunded;
-    
     function voteBlue() public payable {
-         addressToAmountFunded[msg.sender] + msg.value;
          Voter storage sender = _voters[msg.sender];
          totalBlue++;
          sender.votedBlue=true;
          TotalPaidToken +=PRICE;
 
     }
-    
-    function voteGold() public {
+
+    function voteGold() public payable {
          Voter storage sender = _voters[msg.sender];
          totalGold++;
          sender.votedGold=true;
          TotalPaidToken +=PRICE;
     }
-    
+
     function winner_share(uint256 whichColor) public {
         Owner storage chairperson = _owner[msg.sender];
         require(msg.sender==_chairperson,"U are not owner.");
@@ -65,7 +63,7 @@ contract theDress{
             chairperson.goldWon=false;
         }
     }
-    
+
     function withdraw() public {
         Owner storage chairperson = _owner[msg.sender];
         Voter storage sender = _voters[msg.sender];
@@ -76,7 +74,7 @@ contract theDress{
          //transfer totelpaidtoken
         }
         else{
-        require(!sender.votedBlue,"Gold is won but u are not woted gold!");
+        require(!sender.votedBlue,"Blue is won but u are not woted gold!");
         total_ownerPercentage = TotalPaidToken * OWNER_PERCENTAGE/100;
         TotalPaidToken = total_ownerPercentage / (TotalPaidToken * 100);
         //transfer totelpaidtoken
