f07042364c2aca6aba0d590f507e954728af31c6
Eren Yılmaz <42775426+sherzed@users.noreply.github.com>
2022-06-20 11:55:44 +0300

    Update theDress.sol

--- theDress.sol
+++ theDress.sol
@@ -1,57 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract theDress{
-     uint256 STEP = 5 minutes;
-    // IERC20 private dressToken;
-     uint256 public immutable_start_time;
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract theDress is Ownable {
+    mapping(address => uint256) public userGold;
+    mapping(address => uint256) public userBlue;
+    uint256 public constant PRICE = 1 ether / 100;
+    uint256 public constant STEP = 5 minutes;
+    uint256 public ownerShare;
+    uint256 public winnerShare;
+    uint256 public constant OWNER_PERCENTAGE = 40;
+    uint256 private goldOrBlue = 2;
+    uint256 private constant GOLD = 0;
+    uint256 private constant BLUE = 1;
+    uint256 public totalGold;
+    uint256 public totalBlue;
+    uint256 timeDifference;
+    uint256 total_ownerPercentage;
+    uint256 public immutable start_time;
+    address _chairperson = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
-     constructor(/*address tokenAddress*/) {
-        //  dressToken = IERC20(tokenAddress);
-         immutable_start_time = block.timestamp;
-         PRICE = 1 ether/1000;
-     }
+    error InsufficientFunds();
+    error InvalidWinnerID();
+    error TransferTxError();
+
+    constructor() {
+        start_time = block.timestamp;
+    }
 
      struct Voter {
         uint vote;
         bool votedBlue;
         bool votedGold;
     }
-    
+    mapping(address => Voter) private _voters;
+
     struct Owner {
         bool blueWon;
         bool goldWon;
     }
-
-    mapping(address => Voter) private _voters;
     mapping(address => Owner) private _owner;
-    uint256 [2] voters;
-    uint256 public totalGold;
-    uint256 public totalBlue;
-    uint256 public PRICE = 1 ether/1000;
-    uint256 private OWNER_PERCENTAGE = 40;
-    uint256 public TotalPaidToken;
-    uint256 passingTime;
-    uint256 timeDifference;
-    uint256 total_ownerPercentage;
-    address _chairperson = 0x6a411Be2a84eaf31d9F6092CA08F364Fb9Fe1350;
 
-    function voteBlue() public payable {
-         Voter storage sender = _voters[msg.sender];
-         totalBlue++;
-         sender.votedBlue=true;
-         TotalPaidToken +=PRICE;
-
+    function calculatePrice() internal view returns (uint256) {
+        uint256 timeDif = block.timestamp - start_time;
+        return PRICE + (timeDif / STEP) * 0.00001 ether;
     }
 
-    function voteGold() public payable {
-         Voter storage sender = _voters[msg.sender];
-         totalGold++;
-         sender.votedGold=true;
-         TotalPaidToken +=PRICE;
+    function blue(uint256 quantity) external payable {
+        uint256 enterPrice = calculatePrice();
+        uint256 fullPrice = quantity * enterPrice;
+        if (msg.value < fullPrice) revert InsufficientFunds();
+        Voter storage sender = _voters[msg.sender];
+        totalBlue += quantity;
+        userBlue[msg.sender] += quantity;
+        sender.votedBlue=true;
+        updateShares();
     }
 
-    function winner_share(uint256 whichColor) public {
+    function updateShares() private {
+        ownerShare += (msg.value * OWNER_PERCENTAGE) / 100;
+        winnerShare += (msg.value * (100 - OWNER_PERCENTAGE)) / 100;
+    }
+
+    function gold(uint256 quantity) public payable {
+        uint256 enterPrice = calculatePrice();
+        uint256 fullPrice = quantity * enterPrice;
+        if (msg.value < fullPrice) revert InsufficientFunds();
+        userGold[msg.sender] += quantity;
+        totalGold += quantity;
+        Voter storage sender = _voters[msg.sender];
+        sender.votedGold=true;
+        updateShares();
+    }
+
+    function winner(uint256 whichColor) public {
         Owner storage chairperson = _owner[msg.sender];
         require(msg.sender==_chairperson,"U are not owner.");
         if(whichColor==1){
@@ -64,20 +85,30 @@ contract theDress{
         }
     }
 
-    function withdraw() public {
+    function ownerWithdraw() external onlyOwner {
+        uint256 share = ownerShare;
+        ownerShare = 0;
+        (bool isSuccess, ) = payable(owner()).call{ value: share }("");
+        if (!isSuccess) revert TransferTxError();
+    }
+
+    function winnerWithdraw() external {
+        uint256 withdrawAmount;
         Owner storage chairperson = _owner[msg.sender];
+        require(!chairperson.blueWon||!chairperson.goldWon,"Any dresses has not win!");
         Voter storage sender = _voters[msg.sender];
-        if(chairperson.goldWon==true){
-        require(!sender.votedGold,"Gold is won but u are not woted gold!");
-        total_ownerPercentage = TotalPaidToken * OWNER_PERCENTAGE/100;
-        TotalPaidToken = total_ownerPercentage / (TotalPaidToken * 100);
-         //transfer totelpaidtoken
+        require(!sender.votedGold||!sender.votedBlue,"You didn't vote for the winning dress!");
+        if(chairperson.blueWon){
+            withdrawAmount = (winnerShare * userBlue[msg.sender]) / totalGold;
+            userBlue[msg.sender] = 0;
         }
-        else{
-        require(!sender.votedBlue,"Blue is won but u are not woted gold!");
-        total_ownerPercentage = TotalPaidToken * OWNER_PERCENTAGE/100;
-        TotalPaidToken = total_ownerPercentage / (TotalPaidToken * 100);
-        //transfer totelpaidtoken
+        if(chairperson.goldWon){
+            withdrawAmount = (winnerShare * userGold[msg.sender]) / totalGold;
+            userGold[msg.sender] = 0;
         }
+         (bool isSuccess, ) = payable(msg.sender).call{ value: withdrawAmount }(
+            ""
+        );
+        if (!isSuccess) revert TransferTxError();
     }
 }
