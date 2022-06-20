fe427ea6d265f0a02f916d2b074715f0f3695628
Eren Yılmaz <42775426+sherzed@users.noreply.github.com>
2022-06-20 17:40:35 +0300

    Update theDress.sol

--- theDress.sol
+++ theDress.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract theDress is Ownable {
-    uint256 public constant PRICE = 1 ether / 100;
-    uint256 public constant STEP = 5 minutes;
-    uint256 private constant GOLD = 0;
-    uint256 private constant BLUE = 1;
-    uint256 public constant OWNER_PERCENTAGE = 40;
-    uint256 public ownerShare;
-    uint256 public winnerShare;
     uint256 public totalGold;
     uint256 public totalBlue;
+    mapping(address => uint256) public userGold;
+    mapping(address => uint256) public userBlue;
+    uint256 public constant PRICE = 1 ether / 100;
+    uint256 public constant STEP = 5 minutes;
+    uint256 public ownerShare;
+    uint256 public winnerShare;
+    uint256 public constant OWNER_PERCENTAGE = 40;
     uint256 private goldOrBlue = 2;
+    uint256 private constant GOLD = 0;
+    uint256 private constant BLUE = 1;
+
     uint256 public immutable start_time;
-    uint256 timeDifference;
-    uint256 total_ownerPercentage;
-    address Owner;
 
     error InsufficientFunds();
     error InvalidWinnerID();
@@ -23,29 +24,11 @@ contract theDress is Ownable {
 
     constructor() {
         start_time = block.timestamp;
-        Owner = msg.sender;
     }
-
-     struct Voter {
-        uint gold;
-        uint blue;
-    }
-
-    mapping(address => Voter) public _voters;
 
     function calculatePrice() internal view returns (uint256) {
         uint256 timeDif = block.timestamp - start_time;
         return PRICE + (timeDif / STEP) * 0.00001 ether;
-    }
-
-    function blue(uint256 quantity) external payable {
-        uint256 enterPrice = calculatePrice();
-        uint256 fullPrice = quantity * enterPrice;
-        if (msg.value < fullPrice) revert InsufficientFunds();
-        Voter storage sender = _voters[msg.sender];
-        totalBlue += quantity;
-        sender.blue+=quantity;
-        updateShares();
     }
 
     function updateShares() private {
@@ -53,18 +36,28 @@ contract theDress is Ownable {
         winnerShare += (msg.value * (100 - OWNER_PERCENTAGE)) / 100;
     }
 
-    function gold(uint256 quantity) public payable {
+    function gold(uint256 quantity) external payable {
         uint256 enterPrice = calculatePrice();
         uint256 fullPrice = quantity * enterPrice;
         if (msg.value < fullPrice) revert InsufficientFunds();
+
+        userGold[msg.sender] += quantity;
         totalGold += quantity;
-        Voter storage sender = _voters[msg.sender];
-        sender.gold+=quantity;
         updateShares();
     }
 
-    function winner(uint256 winnerId) public {
-        require(msg.sender==Owner,"U are not owner.");
+    function blue(uint256 quantity) external payable {
+        uint256 enterPrice = calculatePrice();
+        uint256 fullPrice = quantity * enterPrice;
+        if (msg.value < fullPrice) revert InsufficientFunds();
+
+        userBlue[msg.sender] += quantity;
+        totalBlue += quantity;
+        updateShares();
+    }
+
+    function winner(uint256 winnerId) external onlyOwner {
+        if (winnerId != GOLD && winnerId != BLUE) revert InvalidWinnerID();
         goldOrBlue = winnerId;
     }
 
@@ -77,16 +70,16 @@ contract theDress is Ownable {
 
     function winnerWithdraw() external {
         uint256 withdrawAmount;
-        require(goldOrBlue!=2,"Any dresses has not win!");
-        Voter storage sender = _voters[msg.sender];
-        if (goldOrBlue == 0) {
-            withdrawAmount = (winnerShare * sender.gold) / totalGold;
-            sender.gold = 0;
+        if (goldOrBlue == GOLD) {
+            withdrawAmount = (winnerShare * userGold[msg.sender]) / totalGold;
+
+            userGold[msg.sender] = 0;
         } else {
-            withdrawAmount = (winnerShare * sender.blue) / totalBlue;
-            sender.blue = 0;
+            withdrawAmount = (winnerShare * userBlue[msg.sender]) / totalBlue;
+
+            userBlue[msg.sender] = 0;
         }
-         (bool isSuccess, ) = payable(msg.sender).call{ value: withdrawAmount }(
+        (bool isSuccess, ) = payable(msg.sender).call{ value: withdrawAmount }(
             ""
         );
         if (!isSuccess) revert TransferTxError();
