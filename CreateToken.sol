50d5ae1b3e2173f5af4e2c28966963a396dc94a3
Eren Yılmaz <42775426+sherzed@users.noreply.github.com>
2022-06-28 15:36:26 +0300

    Update CreateToken.sol

--- CreateToken.sol
+++ CreateToken.sol
@@ -3,11 +3,22 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CreatedToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Bixos Air Conditioing", "BSXAIR") {
+    constructor(uint256 initialSupply) ERC20("Blame", "BLAME") {
         _mint(_msgSender(), initialSupply);
     }
+    
+    struct Claimer{
+        bool claimed;
+    }
+    mapping(address => Claimer) public userClaimed;
 
     function decimals() public pure override returns (uint8) {
         return 6;
     }
+    address[] wallets;
+    function claimBlame() public {
+        Claimer storage user = userClaimed[msg.sender];
+        require(!user.claimed,"already cleamed");
+        transfer(msg.sender,50000000);
+    }  
 }
